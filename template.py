174272a4cadb32a26ede11bec39fed2df07c3fef
thawtar <mr.thaw.tar1990@gmail.com>
2024-08-24 08:36:47 +0900

    added definitions for meshing

--- template.py
+++ template.py
@@ -14,7 +14,7 @@ list_of_files = [
     f"{project_name}/tests/__init__.py",
     f"{project_name}/config/__init__.py",
     f"{project_name}/docs/__init__.py",
-    f"{project_name}/pipeline/__init__.py",
+    #f"{project_name}/pipeline/__init__.py",
     f"{project_name}/constants/__init__.py",
     "config/config.yaml",
     #"requirements.txt",
