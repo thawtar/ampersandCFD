e8ee444143d669be57149b97e6aafc8122254c42
thawtar <mr.thaw.tar1990@gmail.com>
2024-11-04 11:01:45 +0900

    removed unnecessary files

--- ampersandCFD/tests/events.py
+++
@@ -1,6 +0,0 @@
-class events:
-    def __init__(self):
-        self.events = []
-
-    def openBtn(self):
-        print("Open button clicked")
--- test-codes/app.py
+++
@@ -1,8 +0,0 @@
-from core import ampersand
-
-def main(server=None, **kwargs):
-    app = ampersand(server)
-    app.server.start(**kwargs)
-
-if __name__ == "__main__":
-    main()
