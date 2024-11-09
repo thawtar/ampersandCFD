f2b7bc913a8c56043740ab13362660af06208c71
thawtar <mr.thaw.tar1990@gmail.com>
2024-11-09 09:17:39 +0900

    adapted main.py for post-processing

--- src/main.py
+++ src/main.py
@@ -20,6 +20,7 @@
 from primitives import ampersandIO
 from create_project import create_project
 from open_project import open_project
+from watch_sim import watch_sim
 #from headers import get_ampersand_header
 #import os
 import argparse
@@ -28,6 +29,7 @@ def main():
     parser = argparse.ArgumentParser(description='Ampersand CFD Automation Tool')
     parser.add_argument('--create', action='store_true', help='Create a new project')
     parser.add_argument('--open', action='store_true', help='Open an existing project')
+    parser.add_argument('--post', action='store_true', help='Post-process the simulation')
     args = parser.parse_args()
 
     #os.system('cls' if os.name == 'nt' else 'clear')
@@ -44,6 +46,14 @@ def main():
     elif args.open:
         try:
             open_project()
+        except KeyboardInterrupt:
+            ampersandIO.printMessage("\nKeyboardInterrupt detected! Aborting project creation")
+            exit()
+        except Exception as error:
+            ampersandIO.printError(error)
+    elif args.post:
+        try:
+            watch_sim()
         except KeyboardInterrupt:
             ampersandIO.printMessage("\nKeyboardInterrupt detected! Aborting project creation")
             exit()
